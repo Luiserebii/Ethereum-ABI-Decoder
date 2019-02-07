49f0cd696eda7174baa84c4d7df6e01dcb306d9a
Luiserebii <luis@serebii.io>
2019-02-06 22:07:21 -0500

    Added "ABIDecoder.java". Fleshing out basic methods

---
+++ src/main/ABIDecoder.java
@@ -0,0 +1,26 @@
+package main;
+
+public class ABIDecoder {
+
+	public ABIDecoder(){
+
+	}
+
+
+	public String toCleanFunctionSig(String function){
+
+	    String cleanSig = "";
+	    String functionName;
+	    String[] parameterTypes;
+
+		return cleanSig;
+	};
+
+	public String toMethodID(String functonSig){
+
+		String methodID = "";
+
+		return methodID;
+	};
+
+}
